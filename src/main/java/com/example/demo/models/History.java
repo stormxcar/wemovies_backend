62c0011ac6341909ec1bac880be025fc7d01f6d0
nguyen kha <nkha3561@gmail.com>
2025-05-26 21:06:26 +0700

    fix pom

--- src/main/java/com/example/demo/models/History.java
+++ src/main/java/com/example/demo/models/History.java
@@ -1,44 +1,44 @@
-package com.example.demo.models;
-
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-@Data
-@Table(name = "history")
-public class History {
-     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "user_id", nullable = false)
-    private String userId;
-
-    @Column(name = "movie_id", nullable = false)
-    private String movieId;
-
-    @Column(name = "pause_time")
-    private Double pauseTime;
-
-    @Column(name = "last_watched")
-    private LocalDateTime lastWatched;
-    
-
-    public History(String userId, String movieId, Double pauseTime) {
-        this.userId = userId;
-        this.movieId = movieId;
-        this.pauseTime = pauseTime;
-        this.lastWatched = LocalDateTime.now();
-    }
-}
+//package com.example.demo.models;
+//
+//import java.time.LocalDateTime;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//@Data
+//@Table(name = "history")
+//public class History {
+//     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "user_id", nullable = false)
+//    private String userId;
+//
+//    @Column(name = "movie_id", nullable = false)
+//    private String movieId;
+//
+//    @Column(name = "pause_time")
+//    private Double pauseTime;
+//
+//    @Column(name = "last_watched")
+//    private LocalDateTime lastWatched;
+//
+//
+//    public History(String userId, String movieId, Double pauseTime) {
+//        this.userId = userId;
+//        this.movieId = movieId;
+//        this.pauseTime = pauseTime;
+//        this.lastWatched = LocalDateTime.now();
+//    }
+//}
